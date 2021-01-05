f9d8ffacaff572b839b3a5bed0cf71f8acb491de
5riggulggul <sjy4477@gmail.com>
2021-01-05 22:53:10 +0900

    210105 leetcode

--- leetcode_top_100/3_Longest_Substring_Without_Repeating_Characters.py
+++ leetcode_top_100/3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,16 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        right = 0
-        box = {}
-        maxi = 0
+        left = 0 #구간의 left index
+        right = 0 #구간의 right index
+        box = {} #구간에 존재하는 알파벳 : 가장 최근에 마주쳤을 때의 index
+        maxi = 0 # 구간 길이 최댓값
         if len(s) == 0:
             return maxi
         while right != len(s):
-            if s[right] not in box:
+            if s[right] not in box: #처음보는 알파벳일 경우
                 box[s[right]] = right
-            else:
-                if left < box[s[right]] + 1:
+            else: #알파벳 재출현의 경우
+                if left < box[s[right]] + 1: #전의 알파벳 index가 left작으면 구간에 어차피 포함x -> left 그대로
                     left = box[s[right]] + 1
                 box[s[right]] = right
             if (right - left + 1) > maxi:
