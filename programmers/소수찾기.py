21dbdbd39f8a1c89d1118c59cb450093ee1d3eb3
5riggulggul <sjy4477@gmail.com>
2021-01-15 23:54:08 +0900

    210115 programmers

--- programmers/소수찾기.py
+++ programmers/소수찾기.py
@@ -1,9 +1,31 @@
-numbers = "011"
-result =[]
-index = [0] * 5
-temp = []
-def recur(i):
-    temp.append(numbers[i])
-    
-recur(0)
-print(result)
+import math
+def solution(numbers):
+    result =[]
+    index = [0]*1000001
+    temp = []
+    count = 0
+    def recur():
+        for j in range(len(numbers)):
+            if index[j] == 0:
+                index[j] = 1
+                temp.append(numbers[j])
+                result.append(''.join(temp))
+                recur()
+                index[j] = 0
+                del temp[-1]
+
+
+    recur()
+    a1 = list(map(int,result))
+    a2 = set(a1)
+    a1 = list(a2)
+    for i in range(len(a1)):
+        flag = 0
+        for j in range(2,int(math.sqrt(a1[i]))+1):
+            if a1[i] % j == 0:
+                flag = 1
+        if a1[i] == 0 or a1[i] == 1:
+            pass
+        elif flag == 0:
+            count += 1
+    return count
