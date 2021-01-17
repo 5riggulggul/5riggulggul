e04b2237e2df9391b5b91493ee365899fc87f68a
Song Joo Young <sjy4477@gmail.com>
2021-01-18 00:38:26 +0900

    210117 leetcode

--- leetcode_top_100/20_Valid_Parentheses.py
+++ leetcode_top_100/20_Valid_Parentheses.py
@@ -1,3 +1,31 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        tem = list()
+        for i in range(len(s)):
+            if s[i] is '[' or s[i] is '{' or s[i] is '(':
+                tem.append(s[i])
+
+            elif s[i] is ']':
+                if not tem:
+                    return False
+                if tem[-1] is '[':
+                    tem.pop()
+                else: return False
+
+            elif s[i] is '}':
+                if not tem:
+                    return False
+                if tem[-1] is '{':
+                    tem.pop()
+                else: return False
+                
+            elif s[i] is ')':
+                if not tem:
+                    return False
+                if tem[-1] is '(':
+                    tem.pop()
+                else: return False
+        if tem:
+            return False
+        else : return True
         
