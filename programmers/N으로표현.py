4392d3e540ef19e4d2a8ad13c341c84cf3173639
5riggulggul <sjy4477@gmail.com>
2021-01-28 23:49:42 +0900

    210128 programmers

--- programmers/N으로표현.py
+++ programmers/N으로표현.py
@@ -1,3 +1,16 @@
 def solution(N, number):
     answer = 0
-    return answer
+    dp = [set() for _ in range(11)]
+    for i in range(1,9):
+        dp[i-1].add(int(str(N)*i))
+        for j in range(i):
+            for k in dp[j]:
+                for l in dp[i-j-2]:
+                    dp[i-1].add(k + l)
+                    dp[i-1].add(k - l)
+                    dp[i-1].add(k * l)
+                    if l != 0:
+                        dp[i-1].add(k // l)
+            if number in dp[i-1]:
+                return i 
+    return -1
